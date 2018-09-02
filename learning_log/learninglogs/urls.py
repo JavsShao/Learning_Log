51d0b24e1dce038cde40639c761f5dbe7e96e917
Hamdi <Javs_Shao@163.com>
2018-09-02 21:52:43 +0800

    创建其他页面 之 显示所有主题的页面。 (模板继承，渲染模板)

--- learning_log/learninglogs/urls.py
+++ learning_log/learninglogs/urls.py
@@ -5,5 +5,7 @@ from . import views
 """定义learninglogs的url模式"""
 urlpatterns = [
     # 主页
-    url(r'^$',views.index),
+    url(r'^$',views.index,name='index'),
+    url(r'^topics/$',views.topics,name='topics'),
+
 ]
