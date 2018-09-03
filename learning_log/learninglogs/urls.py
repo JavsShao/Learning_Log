a25508e0e735969dff8ac89f52f9ac6489c65865
Hamdi <Javs_Shao@163.com>
2018-09-03 16:12:48 +0800

    用户修改条目

--- learning_log/learninglogs/urls.py
+++ learning_log/learninglogs/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     url(r'^new_topic/$',views.new_topic,name='new_topic'),
     # 用于添加新条目的页面
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # 用户可以编辑既有的条目
+    # url(r'^edit_entry/(?P<entry_id>\d+)/$',views.edit_entry, name='edit_entry'),
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,name='edit_entry'),
 ]
--- learning_log/learninglogs/views.py
+++ learning_log/learninglogs/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from .forms import TopicForm,EntryForm
-from .models import Topic
+from .models import Topic,Entry
 
 # Create your views here.
 
@@ -57,3 +57,21 @@ def new_entry(request,topic_id):
             return HttpResponseRedirect(reverse('learninglogs:topic',args=[topic_id]))
     context = {'topic':topic,'form':form}
     return render(request,'learninglogs/new_entry.html',context)
+
+def edit_entry(request,entry_id):
+    """用户可以编辑既有的条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 初次请求，使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learninglogs:topic',args=[topic.id]))
+
+    context = {'entry':entry,'topic':topic,'form':form}
+    return render(request,'learninglogs/edit_entry.html',context)
