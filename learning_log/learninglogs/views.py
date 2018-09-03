7116709c01279dca37911df8f878f8e828355960
Hamdi <Javs_Shao@163.com>
2018-09-03 14:37:27 +0800

    用户可自行添加主题 和 条目

--- learning_log/learninglogs/views.py
+++ learning_log/learninglogs/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .forms import TopicForm
+from .forms import TopicForm,EntryForm
 from .models import Topic
 
 # Create your views here.
@@ -39,3 +39,21 @@ def new_topic(request):
 
     context = {'form':form}
     return render(request,'learninglogs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learninglogs:topic',args=[topic_id]))
+    context = {'topic':topic,'form':form}
+    return render(request,'learninglogs/new_entry.html',context)
